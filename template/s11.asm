9fd4f23c267c6a5b79760cd13dd5c4b45d92c2e7
hasanmehediii <mehedi200105075@gmail.com>
2025-11-04 23:45:02 +0600

    fix s11

--- template/s11.asm
+++ template/s11.asm
@@ -1,55 +1,93 @@
 section .data
-    num db 5
-    msg db 'The factorial is: '
-    len equ $ - msg
+    msg1 db 'Enter a number: ', 0
+    len1 equ $ - msg1
+    msg2 db 'The factorial is: ', 0
+    len2 equ $ - msg2
 
 section .bss
-    fact resb 5 ; To store up to 5 digits for the result and a newline
+    num_str resb 5      ; buffer for user input
+    fact resb 12        ; space for factorial result string
 
 section .text
     global _start
 
 _start:
-    mov al, [num]
-    mov bl, al
-    dec bl
-    mov ah, 0
+    ; -------- Prompt for input --------
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, msg1
+    mov edx, len1
+    int 0x80
+
+    ; -------- Read user input --------
+    mov eax, 3
+    mov ebx, 0
+    mov ecx, num_str
+    mov edx, 5
+    int 0x80
+
+    ; -------- Convert ASCII to integer --------
+    mov eax, 0
+    mov esi, num_str
+next_digit:
+    movzx edx, byte [esi]
+    cmp edx, 10          ; newline?
+    je done_input
+    sub edx, '0'
+    imul eax, eax, 10
+    add eax, edx
+    inc esi
+    jmp next_digit
+done_input:
+    mov [num_str], eax   ; reuse buffer to store integer
+
+    ; -------- Compute factorial --------
+    mov eax, [num_str]   ; n
+    cmp eax, 1
+    jbe factorial_done   ; if n <= 1, result = 1
+
+    mov ebx, eax
+    dec ebx
 
 factorial_loop:
-    mul bl
-    dec bl
-    cmp bl, 0
-    jne factorial_loop
+    mov edx, 0
+    mul ebx
+    dec ebx
+    cmp ebx, 1
+    jg factorial_loop
 
-    ; Convert the result in ax to a string
-    mov edi, fact + 4
-    mov byte [edi], 0xa ; newline
+factorial_done:
+    ; -------- Convert result in EAX to string --------
+    mov edi, fact + 11
+    mov byte [edi], 0x0A
     dec edi
     mov ebx, 10
-
 convert_loop:
     mov edx, 0
     div ebx
     add dl, '0'
     mov [edi], dl
     dec edi
-    cmp eax, 0
-    jne convert_loop
+    test eax, eax
+    jnz convert_loop
+    inc edi
 
-    ; Print the result
+    ; -------- Print "The factorial is: " --------
     mov eax, 4
     mov ebx, 1
-    mov ecx, msg
-    mov edx, len
+    mov ecx, msg2
+    mov edx, len2
     int 0x80
 
+    ; -------- Print factorial result --------
     mov eax, 4
     mov ebx, 1
     mov ecx, edi
-    inc ecx
-    mov edx, fact + 5
+    mov edx, fact + 12
     sub edx, ecx
     int 0x80
 
+    ; -------- Exit --------
     mov eax, 1
+    xor ebx, ebx
     int 0x80
