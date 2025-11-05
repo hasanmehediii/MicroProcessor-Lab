30be76fa0324547ea7ee73c8566d9ff40ff63666
hasanmehediii <mehedi200105075@gmail.com>
2025-11-05 09:36:11 +0600

    s2 fixed

--- template/s2.asm
+++ template/s2.asm
@@ -1,5 +1,5 @@
 section .bss
-    char resb 1
+    char resb 3
 
 section .text
     global _start
@@ -9,15 +9,16 @@ _start:
     mov eax, 3
     mov ebx, 0
     mov ecx, char
-    mov edx, 1
+    mov edx, 3
     int 0x80
 
     ; Print the character
     mov eax, 4
     mov ebx, 1
     mov ecx, char
-    mov edx, 1
+    mov edx, 3
     int 0x80
 
     mov eax, 1
+    mov ebx, 0
     int 0x80
