ec0114734b80ef251b9aa28014cc5879c7f9ae12
hasanmehediii <mehedi200105075@gmail.com>
2026-01-06 15:17:10 +0600

    claude kore dise

--- lab-8/Roll22.asm
+++ lab-8/Roll22.asm
@@ -1,12 +1,14 @@
 section .data
-    buffer times 12 db 0
     space db ' ', 0
     newline db 10, 0
+    minus db '-', 0
 
 section .bss
     n resd 1
     temp resd 1
     root resd 1
+    input_buffer resb 1024
+    output_buffer resb 12
     sign resb 1
     first_output resb 1
     
@@ -16,8 +18,16 @@ section .text
 ; Node structure: [value(4) | left(4) | right(4)] = 12 bytes
 
 _start:
-    ; Read N
-    call read_int
+    ; Read entire input line
+    mov eax, 3           ; sys_read
+    mov ebx, 0           ; stdin
+    mov ecx, input_buffer
+    mov edx, 1024
+    int 0x80
+    
+    ; Parse N from input
+    xor esi, esi         ; buffer index
+    call parse_int
     mov [n], eax
     
     ; Initialize root to NULL
@@ -27,7 +37,7 @@ _start:
     mov ecx, [n]
 .insert_loop:
     push ecx
-    call read_int
+    call parse_int
     mov [temp], eax
     
     ; Allocate node
@@ -71,6 +81,70 @@ _start:
     mov eax, 1
     xor ebx, ebx
     int 0x80
+
+; Parse integer from input_buffer at position esi
+; Returns value in eax, updates esi
+parse_int:
+    push ebx
+    push ecx
+    push edx
+    
+    xor eax, eax         ; result
+    xor ebx, ebx         ; sign (0=positive, 1=negative)
+    
+    ; Skip whitespace
+.skip_space:
+    cmp esi, 1024
+    jge .done
+    movzx ecx, byte [input_buffer + esi]
+    cmp ecx, ' '
+    je .next_char
+    cmp ecx, 10          ; newline
+    je .next_char
+    cmp ecx, 13          ; carriage return
+    je .next_char
+    cmp ecx, 9           ; tab
+    je .next_char
+    jmp .check_sign
+.next_char:
+    inc esi
+    jmp .skip_space
+    
+.check_sign:
+    movzx ecx, byte [input_buffer + esi]
+    cmp ecx, '-'
+    jne .parse_digits
+    mov ebx, 1
+    inc esi
+    
+.parse_digits:
+    cmp esi, 1024
+    jge .apply_sign
+    movzx ecx, byte [input_buffer + esi]
+    
+    ; Check if digit
+    cmp ecx, '0'
+    jl .apply_sign
+    cmp ecx, '9'
+    jg .apply_sign
+    
+    ; Convert and add
+    sub ecx, '0'
+    imul eax, 10
+    add eax, ecx
+    inc esi
+    jmp .parse_digits
+    
+.apply_sign:
+    cmp ebx, 1
+    jne .done
+    neg eax
+    
+.done:
+    pop edx
+    pop ecx
+    pop ebx
+    ret
 
 ; Insert node into BST
 ; Args: [esp+4] = node to insert
@@ -131,13 +205,14 @@ inorder:
     push ebp
     mov ebp, esp
     push ebx
+    push esi
     
-    mov ebx, [ebp+8]
-    cmp ebx, 0
+    mov esi, [ebp+8]     ; Use esi for node pointer
+    cmp esi, 0
     je .done
     
     ; Traverse left
-    push dword [ebx+4]
+    push dword [esi+4]
     call inorder
     add esp, 4
     
@@ -145,86 +220,33 @@ inorder:
     cmp byte [first_output], 1
     je .no_space
     
+    push esi             ; Save node pointer
     mov eax, 4
-    mov ecx, 1
-    mov edx, space
-    mov ecx, edx
+    mov ebx, 1
+    mov ecx, space
     mov edx, 1
     int 0x80
+    pop esi              ; Restore node pointer
     
 .no_space:
     mov byte [first_output], 0
-    mov eax, [ebx]
+    push esi             ; Save node pointer before print
+    mov eax, [esi]       ; Get value to print
     call print_int
+    pop esi              ; Restore node pointer
     
     ; Traverse right
-    push dword [ebx+8]
+    push dword [esi+8]
     call inorder
     add esp, 4
     
 .done:
+    pop esi
     pop ebx
     pop ebp
     ret
 
-; Read integer from stdin
-read_int:
-    push ebx
-    push ecx
-    push edx
-    push esi
-    
-    xor eax, eax         ; result
-    xor ebx, ebx         ; char
-    xor esi, esi         ; sign (0=positive, 1=negative)
-    
-.read_loop:
-    push eax
-    mov eax, 3           ; sys_read
-    mov ebx, 0
-    mov ecx, buffer
-    mov edx, 1
-    int 0x80
-    pop eax
-    
-    cmp eax, 0
-    je .done
-    
-    movzx ebx, byte [buffer]
-    
-    ; Check for minus sign
-    cmp ebx, '-'
-    jne .check_digit
-    mov esi, 1
-    jmp .read_loop
-    
-.check_digit:
-    ; Check if digit
-    cmp ebx, '0'
-    jl .done
-    cmp ebx, '9'
-    jg .done
-    
-    ; Convert and add
-    sub ebx, '0'
-    imul eax, 10
-    add eax, ebx
-    jmp .read_loop
-    
-.done:
-    ; Apply sign
-    cmp esi, 1
-    jne .positive
-    neg eax
-    
-.positive:
-    pop esi
-    pop edx
-    pop ecx
-    pop ebx
-    ret
-
-; Print integer
+; Print integer in eax
 print_int:
     push ebx
     push ecx
@@ -232,45 +254,48 @@ print_int:
     push esi
     push edi
     
-    mov esi, eax
-    mov edi, buffer
-    add edi, 11
-    mov byte [edi], 0
-    dec edi
+    mov edi, eax
+    mov esi, output_buffer
+    add esi, 11          ; Point to end of buffer
+    mov byte [esi], 0
     
     ; Handle negative
-    test esi, esi
+    mov ebx, 0           ; sign flag
+    test edi, edi
     jns .positive
-    neg esi
+    neg edi
+    mov ebx, 1
     
 .positive:
+    ; Convert to string (reverse order)
     mov ecx, 10
     
 .convert_loop:
     xor edx, edx
-    mov eax, esi
+    mov eax, edi
     div ecx
     add dl, '0'
-    mov [edi], dl
-    dec edi
-    mov esi, eax
+    dec esi
+    mov [esi], dl
+    mov edi, eax
     test eax, eax
     jnz .convert_loop
     
-    ; Add minus if negative
-    cmp dword [esp+16], 0
-    jge .print
-    mov byte [edi], '-'
-    dec edi
+    ; Add minus sign if negative
+    test ebx, ebx
+    jz .print
+    dec esi
+    mov byte [esi], '-'
     
 .print:
-    inc edi
-    mov ecx, buffer
+    ; Calculate length
+    mov ecx, output_buffer
     add ecx, 11
-    sub ecx, edi
+    sub ecx, esi
     mov edx, ecx
-    mov ecx, edi
+    mov ecx, esi
     
+    ; Write to stdout
     mov eax, 4
     mov ebx, 1
     int 0x80
